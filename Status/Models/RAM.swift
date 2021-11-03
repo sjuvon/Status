4872cb4030f0b3e282888373dbfa3a347ea8c508
Sjuvon Chung <chung.sjuvon@gmail.com>
2021-11-03 19:50:33 -0400

    Refactoring: Start

--- Status/Models/RAM.swift
+++ Status/Models/RAM.swift
@@ -18,17 +18,7 @@ func rounder(x: Double) -> Double {
 
 
 public struct RAM {
-    /*  Default attributes.
-     
-        N.B.:
-        (1) total = free + speculative + active + inactive + wired + compressor (used by)
-        (2) used = pageable (AKA internal count) - purgeable + wired + compressor
-        (3) 'app memory' = pageable - purgeable
-        (4) 'cached' = external + purgeable
-        (5) Estimates for Memory Pressure:
-            Yellow <~~~> Compressed 2.5 - 4.5 GB
-            Red    <~~~> Compressed 4.5+
-    */
+    /*  Model for RAM statistics */
     let factor: Double = Double( Double(4096) / Double(1024*1024*1024) )
     var display: [String:Double] = [:]
     
@@ -66,110 +56,9 @@ public struct RAM {
         display["percentFI"] = rounder(x: ((temp_free + temp_inactive)/temp_total)*100)
         display["percentWA"] = rounder(x: ((temp_wired + temp_active)/temp_total)*100)
     }
+    
+    
 }
-
-    
-    /*
-    var free: Double
-    var speculative: Double
-    var active: Double
-    var inactive: Double
-    var wired: Double
-    var compressor: Double
-    var pageable: Double
-    var purgeable: Double
-    var app: Double
-    var total: Double
-    var used: Double
-    var percentUsed: Double
-    var percentFree: Double
-    var percentCompressed: Double
-    var percentFI: Double
-    var percentWA: Double
-    */
-    
-    
-    /*
-    public init() {
-        let vm = vm_stat()!
-        // self.listy = shell()
-        
-        
-        let temp_free: Double = Double(vm.free_count)*self.factor
-        let temp_speculative: Double = Double(vm.speculative_count)*self.factor
-        let temp_active: Double = Double(vm.active_count)*self.factor
-        let temp_inactive: Double = Double(vm.inactive_count)*self.factor
-        let temp_wired: Double = Double(vm.wire_count)*self.factor
-        let temp_compressor: Double = Double(vm.compressor_page_count)*self.factor
-        let temp_pageable: Double =  Double(vm.internal_page_count)*self.factor
-        let temp_purgeable: Double = Double(vm.purgeable_count)*self.factor
-        let temp_app: Double = temp_pageable - temp_purgeable
-        let temp_total: Double = temp_free + temp_speculative + temp_active + temp_inactive + temp_wired + temp_compressor
-        let temp_used: Double = temp_app + temp_wired + temp_compressor
-        
-        self.free = rounder(x: temp_free)
-        self.speculative = rounder(x: temp_speculative)
-        self.active = rounder(x: temp_active)
-        self.inactive = rounder(x: temp_inactive)
-        self.wired = rounder(x: temp_wired)
-        self.compressor = rounder(x: temp_compressor)
-        self.pageable = rounder(x: temp_pageable)
-        self.purgeable = rounder(x: temp_purgeable)
-        self.app = rounder(x: temp_app)
-        self.total = rounder(x: temp_total)
-        self.used = rounder(x: temp_used)
-        
-        self.display["used"] = rounder(x: (temp_used/temp_total)*100)
-        self.display["free"] = rounder(x: (temp_free/temp_total)*100)
-        self.display["compressed"] = rounder(x: 100 - ((temp_compressor/temp_total)*100))
-        self.display["FI"] = rounder(x: ((temp_free + temp_inactive)/temp_total)*100)
-        self.display["WA"] = rounder(x: ((temp_wired + temp_active)/temp_total)*100)
-            
-        /*
-        self.percentUsed = rounder(x: (temp_used/temp_total)*100)
-        self.percentFree = rounder(x: (temp_free/temp_total)*100)
-        self.percentCompressed = rounder(x: 100 - ((temp_compressor/temp_total)*100))
-        self.percentFI = rounder(x: ((temp_free + temp_inactive)/temp_total)*100)
-        self.percentWA = rounder(x: ((temp_wired + temp_active)/temp_total)*100)
-        */
-    }
-    
-    
-    public func update() {
-        // :(
-        let vm = vm_stat()!
-        // self.listy = "Try: " + shell()
-        
-        let temp_free: Double = Double(vm.free_count)*self.factor
-        let temp_speculative: Double = Double(vm.speculative_count)*self.factor
-        let temp_active: Double = Double(vm.active_count)*self.factor
-        let temp_inactive: Double = Double(vm.inactive_count)*self.factor
-        let temp_wired: Double = Double(vm.wire_count)*self.factor
-        let temp_compressor: Double = Double(vm.compressor_page_count)*self.factor
-        let temp_pageable: Double =  Double(vm.internal_page_count)*self.factor
-        let temp_purgeable: Double = Double(vm.purgeable_count)*self.factor
-        let temp_app: Double = temp_pageable - temp_purgeable
-        let temp_total: Double = temp_free + temp_speculative + temp_active + temp_inactive + temp_wired + temp_compressor
-        let temp_used: Double = temp_app + temp_wired + temp_compressor
-        
-        self.free = rounder(x: temp_free)
-        self.speculative = rounder(x: temp_speculative)
-        self.active = rounder(x: temp_active)
-        self.inactive = rounder(x: temp_inactive)
-        self.wired = rounder(x: temp_wired)
-        self.compressor = rounder(x: temp_compressor)
-        self.pageable = rounder(x: temp_pageable)
-        self.purgeable = rounder(x: temp_purgeable)
-        self.app = rounder(x: temp_app)
-        self.total = rounder(x: temp_total)
-        
-        self.display["used"] = rounder(x: (temp_used/temp_total)*100)
-        self.display["free"] = rounder(x: (temp_free/temp_total)*100)
-        self.display["compressed"] = rounder(x: 100 - ((temp_compressor/temp_total)*100))
-        self.display["FI"] = rounder(x: ((temp_free + temp_inactive)/temp_total)*100)
-        self.display["WA"] = rounder(x: ((temp_wired + temp_active)/temp_total)*100)
-    }
-    */
 
 
 
--- Status/vm_stat.swift
+++ Status/vm_stat.swift
@@ -10,29 +10,12 @@ import Foundation
 
 
 func vm_stat() -> vm_statistics64? {
-    /*  What needs to be done?
-
-        (1) Call the kernel API
-        (2) Read the API's message
-     
-        The catch: In order to ultimately display API message, need host_statistics64;
-        however, host_statistics64 requires an out-value of integer_t, which
-        vm_statistics64 isn't.  So we fashion a pointer of type integer_t along the way.
- 
-        N.B.: After a good deal of work, finally found the relevant documentation.
-              See: https://www.gnu.org/software/hurd/gnumach-doc/index.html
-    */
-    
-    /*  host_port: The port number of host (me)
-        host_size: Size in integer_t's of vm_statistics64, in preparation for pointer conversion.
-    */
+    /*  Mach kernel API call to retrieve memory status.  */
     let host_port: host_t = mach_host_self()
     var host_size: mach_msg_type_number_t = mach_msg_type_number_t(
         MemoryLayout<vm_statistics64_data_t>.stride / MemoryLayout<integer_t>.stride
     )
     
-    
-    /*  Provide an instance of vm_statistics64 to point to.  */
     var vm_stat: vm_statistics64 = vm_statistics64()
     let pointer_vm_stat = UnsafeMutablePointer<vm_statistics64>.allocate(capacity: 1)
     
@@ -41,13 +24,6 @@ func vm_stat() -> vm_statistics64? {
         count: 1
     )
     
-    
-    /*  Convert pointer, then call host_statistics64.
-     
-        Note: withMemoryRebound only converts original pointer pointer_vm_stat temporarily; the
-        closure is precisely for working in this lifetime.
-        Cf. Alternative solution below.
-     */
     pointer_vm_stat.withMemoryRebound(to: integer_t.self, capacity: 1) {
         host_statistics64(
             host_port,
@@ -59,109 +35,4 @@ func vm_stat() -> vm_statistics64? {
     
     return pointer_vm_stat.pointee
 }
-
-
-    /*  // Alternative solution via bindMemory:
-        // In general, use bindMemory when:
-        // (1) We want the pointer to be converted permanently, and
-        // (2) When pointer_converter has different memory stats (size & stride)
-        //     from the original pointer_vm_stat.
-    
-    let pointer_converter = UnsafeMutableRawPointer(pointer_vm_stat).bindMemory(
-        to: integer_t.self,
-        capacity: Int(host_size)
-    )
-     
-    host_statistics64(
-        host_port,
-        HOST_VM_INFO64,
-        pointer_converter,
-        &host_size)
-    
-    return pointer_vm_stat.pointee
-    */
-
-
-/*
-func vm_list() -> task_basic_info_64 {
-    var targetTask: task_t = mach_task_self_
-    var taskInfoCount: mach_msg_type_number_t = mach_msg_type_number_t(
-        MemoryLayout<task_basic_info_64_data_t>.stride / MemoryLayout<integer_t>.stride
-    )
-    
-    var vm_list: task_basic_info_64 = task_basic_info_64()
-    let pointer_vm_list = UnsafeMutablePointer<task_basic_info_64>.allocate(capacity: 1)
-    
-    pointer_vm_list.initialize(
-        from: &vm_list,
-        count: 1
-    )
-    
-    
-    // Thank you, Nate Cook, at https://stackoverflow.com/questions/27556807/swift-pointer-problems-with-mach-task-basic-info !
-    pointer_vm_list.withMemoryRebound(to: integer_t.self, capacity: 1) {
-        task_info(
-            targetTask,
-            task_flavor_t(TASK_BASIC_INFO_64),
-            $0,
-            &taskInfoCount
-        )
-    }
-    
- 
-    return pointer_vm_list.pointee
-}
-*/
-
-
-/*
-@discardableResult
-func shell() -> String {
-    let task = Process()
-    let pipe = Pipe()
-    
-    task.standardOutput = pipe
-    task.launchPath = "/bin/sh"
-    task.arguments = ["-c", String(format:"%@", "top -o mem")]
-    task.launch()
-    
-    let data = pipe.fileHandleForReading.readDataToEndOfFile()
-    let output = String(data: data, encoding: .utf8)!
-    
-    return output
-}
- */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
